c4e4a3aa4f1a10ef64313663b1ecadfc50e2a937
p1307873 <devnull@localhost>
2016-12-07 21:14:42 +0100

    date correcte

--- Application/src/main/webapp/ressources/JS/home.js
+++ Application/src/main/webapp/ressources/JS/home.js
@@ -331,7 +331,7 @@ function cloneProject(idUser,urlProject,type){
 /** Renvoi la date d'un timetstamp git */
 function getDateGit(timestamp){
     var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
-        yyyy = d.getYear(),
+        yyyy = d.getFullYear(),
         mm = d.getMonth() +1,	// Months are zero based. Add leading 0.
         dd = d.getDate(),			// Add leading 0.
         hh = d.getHours(),
